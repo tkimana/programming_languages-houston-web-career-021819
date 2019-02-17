20a92061a8f7a7df466c2bd9d607383f1b3fb663
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-17 19:38:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,36 @@
 def reformat_languages(languages){
   
 {
-:oo =>{
-  :ruby => {
-    :type =>{"interpreted"}
+:oo => {
+        :ruby => {
+          :type => "interpreted"
+        },
+        :javascript => {
+          :type => "interpreted"
+        },
+        :python => {
+          :type => "interpreted"
+        },
+        :java => {
+          :type => "compiled"
+        }
+      },
+      :functional => {
+        :clojure => {
+          :type => "compiled"
+        },
+        :erlang => {
+          :type => "compiled"
+        },
+        :scala => {
+          :type => "compiled"
+        },
+        :javascript => {
+          :type => "interpreted"
+        }
+     
+      }
+    }
   }
-}
+
 end
